20ca2c62f4f27a2f4a2cecf166082e95ba5c4bd2
Vincent Finn <vincent.s.finn@gmail.com>
2018-12-08 17:09:23 -0500

    set up to modify player controller and camera rig

---
+++ Finishers/Assets/Characters/Player/PlayerMovementInputProcessor.cs
@@ -0,0 +1,73 @@
+using System;
+using UnityEngine;
+
+namespace Finisher.Characters
+{
+    [RequireComponent(typeof (CharacterController))]
+    public class PlayerMovementInputProcessor : MonoBehaviour
+    {
+        private CharacterController character; // A reference to the ThirdPersonCharacter on the object
+        private Transform cam;                  // A reference to the main camera in the scenes transform
+        private Vector3 camForward;             // The current forward direction of the camera
+        private Vector3 moveDirection;          // the world-relative desired move direction, calculated from the camForward and user input.
+        private bool jump;                      
+
+        
+        private void Start()
+        {
+            // get the transform of the main camera
+            if (Camera.main != null)
+            {
+                cam = Camera.main.transform;
+            }
+            else
+            {
+                Debug.LogWarning(
+                    "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
+                // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
+            }
+
+            // get the third person character ( this should never be null due to require component )
+            character = GetComponent<CharacterController>();
+        }
+
+
+        private void Update()
+        {
+            if (!jump)
+            {
+                jump = Input.GetButtonDown("Jump");
+            }
+        }
+
+
+        // Fixed update is called in sync with physics
+        private void FixedUpdate()
+        {
+            // read inputs
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            bool crouch = Input.GetKey(KeyCode.C);
+
+            // calculate move direction to pass to character
+            if (cam != null)
+            {
+                // calculate camera relative direction to move:
+                camForward = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
+                moveDirection = v*camForward + h*cam.right;
+            }
+            else
+            {
+                // we use world-relative directions in the case of no main camera
+                moveDirection = v*Vector3.forward + h*Vector3.right;
+            }
+
+			// if shift is not held, walk
+	        if (Input.GetKey(KeyCode.LeftShift)) moveDirection *= 0.5f;
+
+            // pass all parameters to the character control script
+            character.Move(moveDirection, crouch, jump);
+            jump = false;
+        }
+    }
+}
