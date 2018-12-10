866b868a3059f7a0682e724530f2eb06dc10121d
Vincent Finn <vincent.s.finn@gmail.com>
2018-12-10 16:07:12 -0500

    just added enemy to the same characters namespace

--- Finishers/Assets/Characters/Enemies/Enemy.cs
+++ Finishers/Assets/Characters/Enemies/Enemy.cs
@@ -1,40 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Finisher.Characters;
 
-public class Enemy : MonoBehaviour {
-
-    [SerializeField] float attackRadius = 5f;
-
-    AIMovementController aiCharacterController;
-    GameObject player = null;
-
-	// Use this for initialization
-	void Start () {
-        player = GameObject.FindGameObjectWithTag("Player");
-        aiCharacterController = GetComponent<AIMovementController>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+namespace Finisher.Characters
+{
+    public class Enemy : MonoBehaviour
     {
-        if (player)
-        {
-            pursueNearbyPlayer();
-        }
-    }
 
-    private void pursueNearbyPlayer()
-    {
-        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
-        if (distanceToPlayer <= attackRadius)
+        [SerializeField] float attackRadius = 5f;
+
+        AIMovementController aiCharacterController;
+        GameObject player = null;
+
+        // Use this for initialization
+        void Start()
         {
-            aiCharacterController.SetTarget(player.transform);
+            player = GameObject.FindGameObjectWithTag("Player");
+            aiCharacterController = GetComponent<AIMovementController>();
         }
-        else
+
+        // Update is called once per frame
+        void Update()
         {
-            aiCharacterController.SetTarget(transform);
+            if (player)
+            {
+                pursueNearbyPlayer();
+            }
+        }
+
+        private void pursueNearbyPlayer()
+        {
+            float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+            if (distanceToPlayer <= attackRadius)
+            {
+                aiCharacterController.SetTarget(player.transform);
+            }
+            else
+            {
+                aiCharacterController.SetTarget(transform);
+            }
         }
     }
 }
