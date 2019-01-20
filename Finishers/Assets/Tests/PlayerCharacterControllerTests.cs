4382e625943dec2c7ca6eb9c947a8f5e678d5ab8
Jack Stillwell <jack.stillwell@outlook.com>
2019-01-19 23:43:39 -0500

    commented out a line

--- Finishers/Assets/Tests/PlayerCharacterControllerTests.cs
+++ Finishers/Assets/Tests/PlayerCharacterControllerTests.cs
@@ -66,7 +66,7 @@ namespace Tests
              * - Player attacks
              */
 
-            Master.Player.GetComponent<>();
+            // Master.Player.GetComponent<>();
             
             /*
              * POST CONDITIONS:
