458be31ff83aadbe7b0f722274705c12ca575fba
Mike <mat338@drexel.edu>
2019-01-22 17:34:27 -0500

    Created a new folder for camera animation clips. F1 now togglesthe finisher grab camera state (which will actually occur on grab later).

---
+++ Finishers/Assets/Camera/Scripts/CameraAnimatorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAnimatorController : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        CheckForInputs();
+    }
+
+    private void CheckForInputs()
+    {
+        bool _isGrabbing = animator.GetBool("isGrabbing");
+
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            Debug.Log("Is Grabbing: " + !_isGrabbing);
+            animator.SetBool("isGrabbing", !_isGrabbing);
+        }
+    }
+}
