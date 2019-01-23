0d86aa0c013b8e82c02df73e49790110968164ac
Mike <mat338@drexel.edu>
2019-01-23 00:53:18 -0500

    Added an example transition, toggled by pressing 4.

--- Finishers/Assets/Camera/Scripts/CameraAnimatorController.cs
+++ Finishers/Assets/Camera/Scripts/CameraAnimatorController.cs
@@ -20,12 +20,27 @@ public class CameraAnimatorController : MonoBehaviour
 
     private void CheckForInputs()
     {
-        bool _isGrabbing = animator.GetBool("isGrabbing");
+        var _isGrabbing = animator.GetBool("isGrabbing");
+        var _transition_ID = animator.GetInteger("transition_ID");
 
         if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
         {
             Debug.Log("Is Grabbing: " + !_isGrabbing);
             animator.SetBool("isGrabbing", !_isGrabbing);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            Debug.Log("Current Transition: " + _transition_ID % 2);
+
+            if (_transition_ID != 1)
+            {
+                animator.SetInteger("transition_ID", 1);
+            }
+            else
+            {
+                animator.SetInteger("transition_ID", 2);
+            }
         }
     }
 }
