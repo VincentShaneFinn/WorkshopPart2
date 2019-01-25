5f0ec61888691ccc732a370e28362e2590c364d4
Linfeng Zhou <lz346@drexel.edu>
2019-01-25 16:06:30 -0500

    add enemies state and switch
    use their state to control their behavier

--- Finishers/Assets/Characters/Enemies/EnemyAI.cs
+++ Finishers/Assets/Characters/Enemies/EnemyAI.cs
@@ -4,7 +4,7 @@ using Finisher.Characters.Systems;
 
 namespace Finisher.Characters.Enemies
 {
-    public enum EnemyState { idle, Patrolling, Chasing, Attacking }
+    public enum EnemyState { idle, Patrolling, Suround, Chasing, Attacking }
 
     [DisallowMultipleComponent]
     [RequireComponent(typeof(AICharacterController))]
@@ -60,21 +60,95 @@ namespace Finisher.Characters.Enemies
         // Update is called once per frame
         void Update()
         {
-            // todo make a state machine
+            float distanceToPlayer = Vector3.Distance(combatTarget.transform.position, transform.position);
+            switch (state)
+            {
+                case EnemyState.idle:
+                    if (distanceToPlayer <= chaseRadius)
+                    {
+                        state = EnemyState.Chasing;
+                    }
+                    else
+                    {
+                        ToHomePoint();
+                    }
+                    break;
+                case EnemyState.Attacking:
+                    //TODO: change to observer
+                    if (squadManager && squadManager.ManagerState == ManagerState.Waiting)
+                    {
+                        //CURRENTLY MAJORLY BUGGED WHERE IT CRASHES BUILDS ONLY
+                        squadManager.SendWakeUpCallToEnemies();
+                    }
+                    if (UnityEngine.Random.Range(0, 2) == 0)
+                    {
+                        combatSystem.HeavyAttack();
+                    }
+                    else
+                    {
+                        combatSystem.LightAttack();
+                    }
+                    if (distanceToPlayer > attackRadius)
+                    {
+                        state = EnemyState.Chasing;
+                    }
+                    break;
+                case EnemyState.Chasing:
+                    ToChasing();
+                    if (distanceToPlayer <= attackRadius)
+                    {
+                        state = EnemyState.Attacking;
+                    }
+
+                    break;
+                case EnemyState.Suround:
+                    if (distanceToPlayer > 8.0f)
+                    {
+                        ToChasing();
+                    }
+                    else
+                    {
+                        //transform.RotateAround(combatTarget.transform.position, Vector3.up, 5 * Time.deltaTime);
+                    }
+                    break;
+
+            }
+            /* todo make a state machine
             pursueNearbyPlayer();
             if (!playerState.DyingState.Dying)
             {
                 attackPlayerIfNear();
-            }    
+            }  */  
         }
 
+        private void surround()
+        {
+
+        }
+
+        private void ToHomePoint()
+        {
+            character.SetTarget(transform);
+            character.OptionalDestination = homeTargetPosition;
+            character.UseOptionalDestination = true;
+            if (Vector3.Distance(transform.position, homeTargetPosition) <= .26f && transform.rotation != homeTargetRotation)
+            {
+                transform.rotation = homeTargetRotation;
+                outofhome = false;
+            }
+        }
+
+        private void ToChasing()
+        {
+            character.SetTarget(combatTarget.transform);
+            character.UseOptionalDestination = false;
+            
+        }
+        /*
         private void pursueNearbyPlayer()
         {
             float distanceToPlayer = Vector3.Distance(combatTarget.transform.position, transform.position);
-            if (attackorder)
-            {
-            }
-            else if (distanceToPlayer <= chaseRadius && !outofhome)
+            if (attackorder || (distanceToPlayer <= chaseRadius && !outofhome))
             {
                 character.SetTarget(combatTarget.transform);
                 character.UseOptionalDestination = false;
@@ -117,20 +191,18 @@ namespace Finisher.Characters.Enemies
                 }
             }
         }
+        */
 
         public void AttackByManager()
         {
-            character.SetTarget(combatTarget.transform);
-            character.UseOptionalDestination = false;
+            
             state = EnemyState.Chasing;
-            attackorder = true;
+            //attackorder = true;
         }
 
         public void StopByManager()
         {
-            character.SetTarget(transform);
-            character.OptionalDestination = homeTargetPosition;
-            character.UseOptionalDestination = true;
+            state = EnemyState.idle;
             outofhome = true;
             attackorder = false;
         }
