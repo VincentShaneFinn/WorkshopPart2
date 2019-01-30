6dd3db2a14b5a6f046b69e50fad60cc07e27a136
jazkworld <jazkworld@gmail.com>
2019-01-30 00:21:22 -0500

    added a sword swing field to charactersoundconfig

--- Finishers/Assets/Characters/Scripts/SoundHandler/CharacterSoundConfig.cs
+++ Finishers/Assets/Characters/Scripts/SoundHandler/CharacterSoundConfig.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName ="Finisher/Sound/CharacterSoundConfig")]
 public class CharacterSoundConfig : ScriptableObject
 {
-    [SerializeField] private SimpleAudioEvent footStep; 
+    [SerializeField] private SimpleAudioEvent footStep;
+    [SerializeField] private SimpleAudioEvent swordSwing;
     public SimpleAudioEvent FootStep { get { return footStep; } }
+    public SimpleAudioEvent SwordSwing {get { return swordSwing; } }
 }
