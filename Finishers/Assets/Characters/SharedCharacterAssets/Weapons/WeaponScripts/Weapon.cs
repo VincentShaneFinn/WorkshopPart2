03f06055fb82e8da538f49c062abadb5fa13600c
vince <vsf23@drexel.edu>
2019-02-03 15:01:39 -0500

    trying to get it to toggle, cant get it to look perfect

--- Finishers/Assets/Characters/SharedCharacterAssets/Weapons/WeaponScripts/Weapon.cs
+++ Finishers/Assets/Characters/SharedCharacterAssets/Weapons/WeaponScripts/Weapon.cs
@@ -1,18 +1,61 @@
 ﻿using UnityEngine;
 
+using Finisher.Characters.Systems;
+using System.Collections;
+
 namespace Finisher.Characters.Weapons
 {
     public class Weapon : MonoBehaviour
     {
 
         [SerializeField] private GameObject soulEffect;
+        [SerializeField] private ParticleSystem trailEffect;
+
+        private float time = 0;
 
         private WeaponColliderManager colliderManager;
+        private CombatSystem combatSystem;
 
         // Start is called before the first frame update
         void Start()
         {
             colliderManager = GetComponent<WeaponColliderManager>();
+            combatSystem = GetComponentInParent<CombatSystem>();
+
+            if(combatSystem)
+            {
+                combatSystem.OnDamageFrameChanged += ToggleTrailingEffect;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if(combatSystem)
+            {
+                combatSystem.OnDamageFrameChanged -= ToggleTrailingEffect;
+            }
+        }
+
+        private void ToggleTrailingEffect(bool fuck)
+        {
+            print(fuck);
+            if (fuck)
+            {
+                trailEffect.Play();
+                time = Time.time;
+                StartCoroutine(stopPlayer());
+            }
+            else
+            {
+                //StartCoroutine(stopPlayer());
+            }
+
+        }
+
+        IEnumerator stopPlayer()
+        {
+            yield return new WaitForSeconds(.01f);
+            trailEffect.Stop();
         }
 
         public void ToggleSoul(bool enabled)
