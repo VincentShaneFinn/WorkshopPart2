2e52286127d9f69f394a3ea2c76a0a9546f32ad7
whoops <sl3329@drexel.edu>
2019-02-19 17:19:44 -0500

    armor break

---
+++ Finishers/Assets/Characters/Enemies/Systems/ArmorBreak/ArmorBreak.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmorBreak : MonoBehaviour
+{
+    [SerializeField] Joint joint;
+
+    public void breakArmor() {
+        Destroy(joint);
+    }
+}
