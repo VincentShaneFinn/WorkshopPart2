4c59bae602392f5069b00bf86b0fc090eeee540f
Vincent Finn <vincent.s.finn@gmail.com>
2019-02-28 10:17:38 -0500

    fixed some issues with breakable armor, and its a bit more dynamic

---
+++ Finishers/Assets/Characters/Enemies/BreakableParts/BreakableHealthBar.cs
@@ -0,0 +1,45 @@
+﻿using Finisher.Characters.Systems;
+using UnityEngine;
+
+namespace Finisher.Characters.Enemies.BreakableParts
+{
+    public class BreakableHealthBar : MonoBehaviour
+    {
+        private int hp = 3;
+
+        [SerializeField] private BreakablePart breakablePart;
+        private HealthSystem parentHealthSystem = null;
+        void Start()
+        {
+            parentHealthSystem = GetComponentInParent<HealthSystem>();
+            if (parentHealthSystem != null)
+            {
+                parentHealthSystem.OnDamageTaken += hitArmor;
+            }
+        }
+
+        void OnDestroy()
+        {
+            if (parentHealthSystem != null)
+            {
+                parentHealthSystem.OnDamageTaken -= hitArmor;
+            }
+        }
+
+        public void hitArmor()
+        {
+            if(UnityEngine.Random.Range(0,1f) < 0.6f)
+            {
+                hp--;
+            }
+            if (hp <= 0)
+            {
+                gameObject.SetActive(false);
+                breakablePart.gameObject.transform.SetParent(null);
+                breakablePart.gameObject.SetActive(true);
+                parentHealthSystem.OnDamageTaken -= hitArmor;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
