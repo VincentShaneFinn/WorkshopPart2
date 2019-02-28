92ce4798d0d021d70bc15ccebb23057d002de47d
Vincent Finn <vincent.s.finn@gmail.com>
2019-02-28 14:16:18 -0500

    modified sword collider and health regen value

--- Finishers/Assets/Characters/Scripts/Systems/Health/HealthConfig.cs
+++ Finishers/Assets/Characters/Scripts/Systems/Health/HealthConfig.cs
@@ -9,7 +9,7 @@ namespace Finisher.Characters.Systems
         [SerializeField] float maxVolatility = 100f; public float MaxVolatility { get { return maxVolatility; } }
         [SerializeField] int knockbackLimit = 2; public int KnockbackLimit { get { return knockbackLimit; } }
         [SerializeField] float freeKnockbackTime = 1f; public float FreeKnockbackTime { get { return freeKnockbackTime; } }
-        [SerializeField] float regenPerSecond = 5f; public float RegenPerSecond { get { return regenPerSecond; } }
+        [SerializeField] float regenPerSecond = 0f; public float RegenPerSecond { get { return regenPerSecond; } }
 
         [SerializeField] AnimationClip[] normalDeathAnimations = null; public AnimationClip[] NormalDeathAnimations { get { return normalDeathAnimations; } }
         [SerializeField] AnimationClip[] knockbackAnimations = null; public AnimationClip[] KnockbackAnimations { get { return knockbackAnimations; } }
