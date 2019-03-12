cd731413ea05ad424c175ba7d84a9e7f286e6fa3
Mike <mat338@drexel.edu>
2019-03-12 13:48:54 -0400

    Added camera shake script and fixed combo counter display. Also a bunch of meta files for some reason.

---
+++ Finishers/Assets/Camera/Scripts/CameraShake.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour
+{
+
+    public float Strength;
+    public float Duration;
+
+    private Vector3 _initialCameraPosition;
+    private float _remainingShakeTime;
+
+    private void Awake()
+    {
+        _initialCameraPosition = transform.localPosition;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            print("shake");
+            Shake();
+        }
+
+        if (_remainingShakeTime <= 0)
+        {
+            transform.localPosition = _initialCameraPosition;
+            return;
+        }
+
+        transform.Translate(Random.insideUnitCircle * Strength);
+
+        _remainingShakeTime -= Time.deltaTime;
+    }
+
+    public void Shake()
+    {
+        _remainingShakeTime = Duration;
+    }
+
+}
